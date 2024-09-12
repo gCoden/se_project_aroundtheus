70f0d869b3f0df09c10a1da6ec7c320fb396b8f2
gavint <gavinatorthomas2000@gmail.com>
2024-09-11 19:38:09 -0500

    fixed type error add and remove

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,15 +1,15 @@
 function showInpuError(formEl, inputEl, { inputErrorClass, errorClass }) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classlist.add(inputErrorClass);
+  inputEl.classList.add(inputErrorClass);
   errorMessageEl.textcontent = inputEl.validationMessage;
-  errorMessageEl.classlsit.add(errorClass);
+  errorMessageEl.classList.add(errorClass);
 }
 
 function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
   const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-  inputEl.classlist.remove(inputErrorClass);
+  inputEl.classList.remove(inputErrorClass);
   errorMessageEl.textcontent = "";
-  errorMessageEl.classlsit.remove(errorClass);
+  errorMessageEl.classList.remove(errorClass);
 }
 
 function checkInputValidity(formEl, inputEl, options) {
@@ -19,8 +19,8 @@ function checkInputValidity(formEl, inputEl, options) {
   hideInputError(formEl, inputEl, options);
 }
 
-function hasInvalidInput(inputlist) {
-  return !inputlist.every((inputEl) => inputEl.validity.valid);
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
 }
 
 function enableButton() {
