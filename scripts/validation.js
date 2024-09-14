c662184626e6598420e5be035f49b942f19b8542
gavint <gavinatorthomas2000@gmail.com>
2024-09-13 20:48:39 -0500

    enable and disable button responsive

--- scripts/validation.js
+++ scripts/validation.js
@@ -48,7 +48,7 @@ function setEventListeners(formEl, options) {
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
-      toggleButtonState(inputEls, submitButton, options);
+      toggleButtonState(inputEls, options, submitButton);
     });
   });
 }
